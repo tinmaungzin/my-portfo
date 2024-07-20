f17aaffeff7a523199047febc0467dd5550702bb
Tin Maung Zin <tinmaungzin.tmz@gmail.com>
2024-07-20 14:25:50 +0700

    fix: add basePath to nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export"
+  output: "export",
+  basePath: "/my-portfo"
 };
 
 export default nextConfig;
